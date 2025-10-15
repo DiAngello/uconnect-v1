a027ac1e4930be9feb7bba6e053a6dfe4ddbbbd7
Andressa de Souza <andressa.gabrielly.ofc@gmail.com>
2025-10-15 19:14:20 -0300

    Teste chat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,53 +3,12 @@ import Calendario from "./components/calendario";
 import Login from "./components/login";
 import { validateSession, logout as apiLogout } from "./services/api";
 
+import Chat from "./components/Chat";
+
 function App() {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const checkAuthStatus = async () => {
-      try {
-      
-        await validateSession();
-        setAuthenticated(true);
-      } catch (error) {
-        console.error("Sessão inválida ou expirada.");
-        setAuthenticated(false);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    checkAuthStatus();
-  }, []); 
-
-  const handleLoginSuccess = () => {
-    setAuthenticated(true);
-  };
-
-  const handleLogout = async () => {
-    await apiLogout();
-    setAuthenticated(false);
-  };
-
-  if (loading) {
-    return (
-      <div className="d-flex justify-content-center align-items-center" style={{ height: "100vh" }}>
-        <div className="spinner-border text-primary" role="status">
-          <span className="visually-hidden">Carregando...</span>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="App">
-      {authenticated ? (
-        <Calendario onLogout={handleLogout} />
-      ) : (
-        <Login onLoginSuccess={handleLoginSuccess} />
-      )}
+      <Chat />
     </div>
   );
 }
